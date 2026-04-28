fa407d4450373bbed7bf3f5b23e544da11842dd2
ckso92 <ckso917813@gmail.com>
2026-04-28 09:04:53 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Yes)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+})
+basic.showIcon(IconNames.StickFigure)
